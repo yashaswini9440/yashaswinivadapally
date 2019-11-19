4d6165fd9956a98461adc5d3e3c03c722425edde
Yashaswini <yashaswinivadapally@gmail.com>
2019-11-19 17:35:29 -0500

    Updates

---
+++ precache-manifest.f90e10347aa5b071b6c8ecc7e2019461.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4808a2f60359bb4acd3f",
+    "url": "/yashaswini9440/static/css/main.2e296d69.chunk.css"
+  },
+  {
+    "revision": "4808a2f60359bb4acd3f",
+    "url": "/yashaswini9440/static/js/main.4808a2f6.chunk.js"
+  },
+  {
+    "revision": "063aa9bebf7acb07ffae",
+    "url": "/yashaswini9440/static/js/runtime~main.063aa9be.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/yashaswini9440/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b4bc27fb8a01eceb2ba7cba09b96254e",
+    "url": "/yashaswini9440/index.html"
+  }
+];
