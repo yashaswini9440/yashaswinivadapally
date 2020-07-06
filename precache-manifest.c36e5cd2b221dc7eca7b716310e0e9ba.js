6dd56c46d35e5961286a93b8de8767b6d62d607d
Yashaswini <yashaswinivadapally@gmail.com>
2020-07-06 16:25:33 -0400

    Updates

---
+++ precache-manifest.c36e5cd2b221dc7eca7b716310e0e9ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b8df94813838e042813",
+    "url": "/yashaswinivadapally/static/css/main.ddd00ec7.chunk.css"
+  },
+  {
+    "revision": "4b8df94813838e042813",
+    "url": "/yashaswinivadapally/static/js/main.4b8df948.chunk.js"
+  },
+  {
+    "revision": "e90ebdcff1483cce38e1",
+    "url": "/yashaswinivadapally/static/js/runtime~main.e90ebdcf.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/yashaswinivadapally/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "6956f0fcf845f3c5f0cf22deb384e2ec",
+    "url": "/yashaswinivadapally/index.html"
+  }
+];
